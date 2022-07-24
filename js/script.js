683a72fbf2341796ef9bae91bde8b53c51f14a43
Dev.Jin <nara04040@naver.com>
2022-07-24 23:01:17 +0900

    test

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
-window.onload = function(){
-  
+$(window).ready(function () {
+    let header = $('.header');
+    let gnb = $('.gnb');
+    let depth2 = $('.depth2')
+
+    header.mouseenter(function () {})
+})
+
+window.onload = function () {
+
 }
