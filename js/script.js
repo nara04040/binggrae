2e36847e88203e02ce5ae2835988f300261e0d2d
Dev.Jin <nara04040@naver.com>
2022-07-24 23:57:18 +0900

    [header] 메뉴내리기

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 $(window).ready(function () {
+    let mainTop = $('.main-top');
     let header = $('.header');
     let gnb = $('.gnb');
-    let depth2 = $('.depth2')
+    let depth_1 = $('.depth1');
+    let depth_2 = $('.depth2');
+    let mainTopMaxHeight = mainTop.outerHeight();
+    let headerMinheight = header.outerHeight();
 
-    header.mouseenter(function () {})
+    mainTop.css('height', headerMinheight);
+    depth_1.mouseenter(function () {
+        mainTop.css('height', mainTopMaxHeight);
+    });
+    mainTop.mouseleave(function () {
+        mainTop.css('height', headerMinheight)
+    });
+
 })
 
 window.onload = function () {
