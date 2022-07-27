ae3b74d2eaf813e25dbd956191ef31a7684425fb
Dev.Jin <nara04040@naver.com>
2022-07-27 09:38:16 +0900

    [html] all change

--- js/script.js
+++ js/script.js
@@ -1,19 +1,5 @@
-$(window).ready(function () {
-    let mainTop = $('.main-top');
-    let header = $('.header');
-    let gnb = $('.gnb');
-    let depth_1 = $('.depth1');
-    let depth_2 = $('.depth2');
-    let mainTopMaxHeight = mainTop.outerHeight();
-    let headerMinheight = header.outerHeight();
+$(document).ready(function () {
 
-    mainTop.css('height', headerMinheight);
-    depth_1.mouseenter(function () {
-        mainTop.css('height', mainTopMaxHeight);
-    });
-    mainTop.mouseleave(function () {
-        mainTop.css('height', headerMinheight)
-    });
 
 })
 
